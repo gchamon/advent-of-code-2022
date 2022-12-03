10de7765bed9faf2b0a4f0cf7cc9320e90e64148
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2022-12-03 16:18:29 -0300

    first day solution

---
+++ day1/solution.py
@@ -0,0 +1,22 @@
+with open("day1/first_puzzle_input.txt") as input_fp:
+    puzzle_input = input_fp.read()
+    elves_raw = puzzle_input.split("\n\n")
+    elves_split_str = (elf.split("\n") for elf in elves_raw)
+    elves_food_calories = ([int(elf_food_calories_str) for elf_food_calories_str in elf]
+                           for elf in elves_split_str)
+    elves_calories_total = (sum(elf_food_calories)
+                            for elf_food_calories in elves_food_calories)
+    sorted_elves_calories = sorted(enumerate(elves_calories_total),
+                                   key=lambda x: x[1],
+                                   reverse=True)
+
+
+def first_puzzle_solution():
+    elf_with_most_calories = sorted_elves_calories[0]
+    print(elf_with_most_calories)
+
+
+def second_puzzle_solution():
+    top_three_elves = sorted_elves_calories[:3]
+    top_three_elves_calories = list(zip(*top_three_elves))[1]
+    print(sum(top_three_elves_calories))
---
+++ main.py
@@ -0,0 +1,4 @@
+import day1.solution
+
+day1.solution.first_puzzle_solution()
+day1.solution.second_puzzle_solution()
